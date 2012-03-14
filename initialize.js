64bb06ecbc62d269ee39f517f0773a3cee28cff3
Kevin <kevin.lai@yale.edu>
2012-03-14 16:41:43 -0400

    Initialize files separated

---
+++ initialize.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+new Accordian('basic-accordian',5,'header_highlight');
+dispO(openRests,'rest',CURTIME);
+dispO(openLibs,'libs',CURTIME);
+dispO(openDHs,'DHs',CURTIME);
+dispO(openFacs,'facs',CURTIME);
+dispC(closedRests,'crest',CURTIME);
+dispC(closedLibs,'clibs',CURTIME);
+dispC(closedDHs,'cDHs',CURTIME);
+dispC(closedFacs,'cfacs',CURTIME);
+})
+
